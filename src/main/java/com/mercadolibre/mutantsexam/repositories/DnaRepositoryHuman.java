9cec217ebfb96d9cc4880b84edfb8602e32e7731
villcadiego <villcadiego@gmail.com>
2018-08-03 14:17:57 -0300

    the mongodb connection has changed

---
+++ src/main/java/com/mercadolibre/mutantsexam/repositories/DnaRepositoryHuman.java
@@ -0,0 +1,9 @@
+package com.mercadolibre.mutantsexam.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.mercadolibre.mutantsexam.model.DnaHuman;
+
+public interface DnaRepositoryHuman extends CrudRepository<DnaHuman, String>{
+
+}
--- src/main/java/com/mercadolibre/mutantsexam/services/DBService.java
+++ src/main/java/com/mercadolibre/mutantsexam/services/DBService.java
@@ -1,11 +1,11 @@
 package com.mercadolibre.mutantsexam.services;
 
-import com.mercadolibre.mutansexam.model.Dna;
 import com.mercadolibre.mutantsexam.exceptions.MutantDBException;
+import com.mercadolibre.mutantsexam.model.DnaHuman;
 
 public interface DBService {
 
-    public void addDNASequence(Dna dna) throws MutantDBException;
+    public void addDNASequence(DnaHuman dna) throws MutantDBException;
     public long getCountMutantsDNA() throws MutantDBException;
     long getTotal() throws MutantDBException;
 }
